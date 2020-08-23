8c06a6446bc5fb79eb921b22696447acdabb4c89
NickFS <nickolas.t.foster@gmail.com>
2020-08-22 21:43:09 -0600

    array questions implemented

--- script.js
+++ script.js
@@ -12,10 +12,45 @@ var answerOptions = document.getElementById("answers");
 var questionNumber = -1
 var answer;
 
+//Quiz Questions
 var questions = [
     {
-        title: "What is the correct syntax for referring to an external script called "xxx.js"?",
-        choices: ["<script href="xxx.js">", "<script name="xxx.js">", "<script src="xxx.js">"],
-        answer: "<script src="xxx.js">"
+        title: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+
+    {
+        title: "Who is making the Web standards?",
+        choices: ["Mozilla", "Microsoft", "Google", "The World Wide Web Consortium"],
+        answer: "The World Wide Web Consortium"
+    },
+
+    {
+        title: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+        choices: ["longdesc", "src", "title", "alt"],
+        answer: "alt"
+    }, 
+
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javascript>", "<script>", "<scripting>", "<js>"],
+        answer: "<script>"
+    },
+
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onmouseover", "onmouseclick", "onclick", "onchange"],
+        answer: "onclick"
+    },
+
+    {
+        title: "Which operator is used to assign a value to a variable?",
+        choices: ["x", "!", "&", "="],
+        answer: "="
     }
 ];
+
+Function(){
+    
+}
