b1a15db12acbeaf1a0cfcfcb830ff104331de81a
NickFS <nickolas.t.foster@gmail.com>
2020-08-23 14:02:49 -0600

    html and styling updates to reflect mock-up

---
+++ questions.js
@@ -0,0 +1,39 @@
+//Quiz Questions
+var questions = [
+    {
+        title: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+
+    {
+        title: "Who is making the Web standards?",
+        choices: ["Mozilla", "Microsoft", "Google", "The World Wide Web Consortium"],
+        answer: "The World Wide Web Consortium"
+    },
+
+    {
+        title: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+        choices: ["longdesc", "src", "title", "alt"],
+        answer: "alt"
+    }, 
+
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javascript>", "<script>", "<scripting>", "<js>"],
+        answer: "<script>"
+    },
+
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onmouseover", "onmouseclick", "onclick", "onchange"],
+        answer: "onclick"
+    },
+
+    {
+        title: "Which operator is used to assign a value to a variable?",
+        choices: ["x", "!", "&", "="],
+        answer: "="
+    }
+];
+
